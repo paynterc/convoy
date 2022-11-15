32c608075a191e327847aec1727e78a89f29712c
Chris Paynter <paynterc@mac.com>
2022-11-14 21:57:02 -0800

    Camera changes
    Small changes to camera distance.

--- Assets/Scripts/UpdateVertsStart.cs
+++ Assets/Scripts/UpdateVertsStart.cs
@@ -6,7 +6,7 @@ public class UpdateVertsStart : MonoBehaviour
 {
     public Color newColor = Color.gray;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         Color[] colors = new Color[mesh.vertices.Length];
